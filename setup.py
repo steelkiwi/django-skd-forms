c5b9bc5d446487090579b27e2619a28c77fc29bc
Artem Antsyferov <antsyferov@steelkiwi.com>
2016-02-24 10:39:06 +0200

    initial commit with created project structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+import skd_forms
+from setuptools import setup, find_packages
+
+setup(
+    name='django-skd-forms',
+    version=skd_forms.__version__,
+    packages=find_packages(exclude=['skd_forms_tests', 'example_project']),
+    description='Django forms alternative',
+    long_description=open('README.rst').read(),
+    author='SteelKiwi Development',
+    author_email='sales@steelkiwi.com',
+    license='MIT',
+    url='https://github.com/steelkiwi/django-skd-forms',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent'])
